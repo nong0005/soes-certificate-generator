9d73dd8b308636f1bb62a2ba592987d7ee5c9539
Your Name <nong0005@algonquinlive.com>
2026-01-02 16:11:16 -0500

    Update package.json and vite.config.js for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/soes-certificate-generator/', // your repo name
   plugins: [
     tailwindcss(),
     react()
